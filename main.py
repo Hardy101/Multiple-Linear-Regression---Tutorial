8b34fbe4cbba02bd701707269662b36525d96296
Eghosa Ordia <eghordia130@gmail.com>
2024-07-26 18:32:49 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# Importing Libraries
+import numpy as np
+import matplotlib as plt
+import pandas as pd
+
+# Importing dataset
+df = pd.read_csv('50_Startups.csv')
+X = df.iloc[:, :-1].values
+y = df.iloc[:, -1].values
+
+# Splitting the Dataset
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Encoding the categorical data
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+state_idx = df.columns.get_loc('State')
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [state_idx])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+
