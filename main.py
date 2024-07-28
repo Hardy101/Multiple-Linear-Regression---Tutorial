ae02019b689e22a4bcee2c491fe47b6c5d9afc8d
Eghosa Ordia <eghordia130@gmail.com>
2024-07-28 23:33:24 +0100

    Repository for learning Machine Learning

--- main.py
+++ main.py
@@ -8,10 +8,6 @@ df = pd.read_csv('50_Startups.csv')
 X = df.iloc[:, :-1].values
 y = df.iloc[:, -1].values
 
-# Splitting the Dataset
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-
 # Encoding the categorical data
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
@@ -19,3 +15,11 @@ state_idx = df.columns.get_loc('State')
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [state_idx])], remainder='passthrough')
 X = np.array(ct.fit_transform(X))
 
+# Splitting the Dataset
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Training the model
+from sklearn.linear_model import LinearRegression
+rg = LinearRegression()
+rg.fit(X_train, y_train)
